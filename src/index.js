b6f582b356ac92c6301ddc688223b3edf053ac65
pierre-francois.hau <pierre-francois.hau@etu.univ-orleans.fr>
2020-12-09 11:20:37 +0100

    récupèration de paramètre JS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App(props){
+    const urlParams = new URLSearchParams(window.location.search)
+    if(urlParams.has('idSalle')){
+        var x = urlParams.get('idSalle');
+    }else{
+        var x = "pas de paramètre";
+    }
+
         return (
             <div className="container-fluid">
                 <div className="game-board">
-                    <p>Salut</p>
+                    <p>vous etes devant la salle {x}</p>
                 </div>
             </div>
         );
